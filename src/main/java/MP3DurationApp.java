72e34dd21afab658865a762808403e449afcfc21
Christos Giannoglou <cgiannoglou@atc.gr>
2018-08-20 11:17:34 +0300

    durationCalculator

--- src/main/java/MP3DurationApp.java
+++ src/main/java/MP3DurationApp.java
@@ -1,9 +1,3 @@
-/*
- * @author Christos Giannoglou
- * 
- * 2018 (c) ATC
- * 
- */
 package main.java;
 
 public class MP3DurationApp {
@@ -14,7 +8,8 @@ public class MP3DurationApp {
     dc.setBitRates();
     dc.setSampleRates();
     dc.setSamples();
-    double mp3Duration = dc.mp3Duration("C:\\Users\\cgiannoglou\\Downloads\\Iron Maiden - Fear of the Dark, live Tele2 Arena, Stockholm Sweden 2018-06-01.mp3");
-    System.out.println("The duration of the mp3 file is: "+mp3Duration);
+    long duration = dc.mp3Duration("C:\\Users\\cgiannoglou\\Documents\\Source\\mp3-duration\\tests\\demo - vbr.mp3", false);
+    System.out.println("The duration of the mp3 file is: "+duration);
   }
+
 }

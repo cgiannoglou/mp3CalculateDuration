a40c438b76fa92b6c6d0d1a1bdd1b9827e605590
cgiannoglou <cgiannoglou@eng-cgiannoglou.atc.gr>
2018-08-20 11:03:08 +0300

    MP3DurationApp (updated)

--- src/main/java/MP3DurationApp.java
+++ src/main/java/MP3DurationApp.java
@@ -10,11 +10,11 @@ public class MP3DurationApp {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    DurationCalculator dc1 = new DurationCalculator();
-    dc1.setBitRates();
-    dc1.setSampleRates();
-    dc1.setSamples();
-    double mp3Duration = dc1.mp3Duration("C:\\Users\\cgiannoglou\\Downloads\\Iron Maiden - Fear of the Dark, live Tele2 Arena, Stockholm Sweden 2018-06-01.mp3");
+    DurationCalculator dc = new DurationCalculator();
+    dc.setBitRates();
+    dc.setSampleRates();
+    dc.setSamples();
+    double mp3Duration = dc.mp3Duration("C:\\Users\\cgiannoglou\\Downloads\\Iron Maiden - Fear of the Dark, live Tele2 Arena, Stockholm Sweden 2018-06-01.mp3");
     System.out.println("The duration of the mp3 file is: "+mp3Duration);
   }
 }

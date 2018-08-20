45f5c2e16fd165e9feff17a7683ca5250bda7b07
cgiannoglou <cgiannoglou@eng-cgiannoglou.atc.gr>
2018-08-20 11:13:34 +0300

    durationCalculator (update)

--- src/main/java/DurationCalculator.java
+++ src/main/java/DurationCalculator.java
@@ -1,16 +1,23 @@
+/*
+ * @author Christos Giannoglou
+ * 
+ * 2018 (c) ATC
+ *  
+ */
 package main.java;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DurationCalculator {
-
+  
   private String[] versions = {"2.5", "x", "2", "1"};
   private String[] layers = {"x", "3", "2", "1"};
 
@@ -18,40 +25,7 @@ public class DurationCalculator {
   private Map<String, int[]> sampleRates = new HashMap<String, int[]>();
   private Map<String, int[]> samples = new HashMap<String, int[]>();
 
-  public long skipID3(byte[] buffer) {
-    int id3v2_flags = 0;
-    int footer_size;
-    int tag_size;
-    if (buffer[0] == 0x49 && buffer[1] == 0x44 && buffer[2] == 0x33) { // 'ID3'
-      id3v2_flags = buffer[5];
-    }
-    if ((id3v2_flags & 0x10) != 0) {
-      footer_size = 10;
-    } else {
-      footer_size = 0;
-    }
-
-//    byte z0 = buffer[6];
-//    System.out.println("z0+0x80: "+z0+0x80);
-//    byte z1 = buffer[7];
-//    byte z2 = buffer[8];
-//    byte z3 = buffer[9];
-    
-    int z0 = buffer[6];
-    System.out.println("z0+0x80: "+z0+0x80);
-    int z1 = buffer[7];
-    int z2 = buffer[8];
-    int z3 = buffer[9];
-
-    if ((z0 & 0x80) == 0 && (z1 & 0x80) == 0 && (z2 & 0x80) == 0 && (z3 & 0x80) == 0) {
-      tag_size =
-          ((z0 & 0x7f) * 2097152) + ((z1 & 0x7f) * 16384) + ((z2 & 0x7f) * 128) + (z3 & 0x7f);
-      System.out.println("10+tag_size+footer_size: "+10+tag_size+footer_size);
-      return 10 + tag_size + footer_size;
-    }
-    return 0;
-  }
-
+  
   public void setBitRates() {
     bitRates.put("V1Lx", new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
     bitRates.put("V1L1",
@@ -84,16 +58,38 @@ public class DurationCalculator {
     samples.put("2", new int[] {0, 348, 1152, 576});
   }
 
-
-  public int frameSize(int samples, int layer, int bit_rate, int sample_rate, int paddingBit) {
-    if (layer == 1) {
-      return (((samples * bit_rate * 125 / sample_rate) + paddingBit * 4));
-    } else { // layer 2, 3
-      return (((samples * bit_rate * 125) / sample_rate) + paddingBit);
+  private int skipID3(int[] buffer) {
+    int id3v2_flags = 0;
+    int footer_size;
+    int tag_size;
+    if (buffer[0] == 0x49 && buffer[1] == 0x44 && buffer[2] == 0x33) { // 'ID3'
+      id3v2_flags = buffer[5];
     }
-  }
+    if ((id3v2_flags & 0x10) != 0) {
+      footer_size = 10;
+    } else {
+      footer_size = 0;
+    }
 
-  public int[] parseFrameHeader(byte[] header) {
+    int z0 = buffer[6];
+    int z1 = buffer[7];
+    int z2 = buffer[8];
+    int z3 = buffer[9];
+
+    if ((z0 & 0x80) == 0 && (z1 & 0x80) == 0 && (z2 & 0x80) == 0 && (z3 & 0x80) == 0) {
+      tag_size =
+          ((z0 & 0x7f) * 2097152) + ((z1 & 0x7f) * 16384) + ((z2 & 0x7f) * 128) + (z3 & 0x7f);
+      return 10 + tag_size + footer_size;
+    }
+    return 0;
+  }
+  
+  private double roundDuration(double duration) {
+    duration = Math.round(duration * 1000);
+    return (duration / 1000);
+  }
+  
+  private float[] parseFrameHeader(int[] header) {
     int b1 = header[1];
     int b2 = header[2];
     String simple_version;
@@ -114,7 +110,7 @@ public class DurationCalculator {
     int layer_bits = (b1 & 0x06) >> 1;
     String layer = layers[layer_bits];
 
-    String bit_rate_key = String.format("%s %s", simple_version, layer);
+    String bit_rate_key = "V"+simple_version+"L"+layer;
     int bit_rate_index = (b2 & 0xf0) >> 4;
 
     if (bitRates.containsKey(bit_rate_key)) {
@@ -124,7 +120,11 @@ public class DurationCalculator {
     }
 
     sample_rate_idx = (b2 & 0x0c) >> 2;
+//    if(sample_rate_idx >2) {
+//      sample_rate_idx = 2;
+//    }
 
+    
     if (sampleRates.containsKey(version)) {
       sample_rate = sampleRates.get(version)[sample_rate_idx];
     } else {
@@ -135,117 +135,96 @@ public class DurationCalculator {
 
     padding_bit = (b2 & 0x02) >> 1;
 
-    int[] results = {bit_rate, sample_rate,
+    float[] results = {bit_rate, sample_rate,
         frameSize(sample, Integer.parseInt(layer), bit_rate, sample_rate, padding_bit), sample};
 
     return results;
   }
 
-  public long estimateDuration(int bitRate, int offset, long filesize) {
-    float kbps = (bitRate * 1000) / 8;
-    double datasize = filesize - offset;
-
-    return roundDuration(datasize / kbps);
+  public float frameSize(int samples, int layer, int bit_rate, int sample_rate, int paddingBit) {
+    if (Integer.toString(layer) == "1") {
+//      if(sample_rate == 0) {
+//        return 0;
+//      }
+      return (((samples * bit_rate * 125 / sample_rate) + paddingBit * 4));
+    } else { // layer 2, 3
+//      if(sample_rate == 0) {
+//        return 0;
+//      }
+      return (((samples * bit_rate * 125) / sample_rate) + paddingBit);
+    }
   }
 
-  public long roundDuration(double duration) {
-    return Math.round(duration * 1000) / 1000; // round to nearest ms
-  }
-
-  public long mp3Duration(String filename, boolean cbrEstimate) {
-    long duration = 0;
-    int[] info = null;
+  public double mp3Duration(String filename) {
+    double duration = 0;
+    float[] info = null;
     File f = new File(filename);
     long filesize = f.length();
     System.out.println(filesize);
 
-    byte[] result = null;
     try {
-      //InputStream input = new BufferedInputStream(new FileInputStream(f));
-      FileInputStream input = new FileInputStream(f);
-      result = readAndClose(input);
-      for (int i = 0; i < result.length; i++) {
-        result[i] = (byte) (result[i] & 0xff);
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      BufferedInputStream input = new BufferedInputStream(new FileInputStream(f));
+
+      int read;
+      byte[] buff = new byte[1024];
+      while ((read = input.read(buff)) > 0) {
+        out.write(buff, 0, read);
       }
-      String buffer = null;
-      for (int i = 0; i < 100; i++) {
-        buffer += (byte) (result[i] & 0xff);
+      out.flush();
+      byte[] audioBytes = out.toByteArray();
+      int[] audioBytes1 = new int[(int) filesize];
+      for (int i = 0; i < audioBytes.length; i++) {
+        audioBytes1[i] = audioBytes[i] & 0xFF;
       }
-      //System.out.println(buffer);
-      byte[] buffer1 = buffer.getBytes();
-      if (buffer1.length < 100) {
+
+      int[] buffer = new int[100];
+      if (audioBytes1.length >= 100) {
+        for (int i = 0; i < 100; i++) {
+          buffer[i] = audioBytes1[i];
+        }
+      } else {
         return 0;
       }
-      int offset = 0;
-      offset = (int) (skipID3(buffer1)&0xff);
 
-      while (true) {
-        //System.out.println(offset);
-        //Arrays.binarySearch(result, 0, result.length, offset);
-        for (int i = 0; i < 10; i++) {
-          
-        }
+      int offset = skipID3(buffer);
+      System.out.println(offset);
+      while(true) {
+        int[] buffer1 = new int[10];
         
-        byte[] buffer2 = buffer.getBytes();
-        if(buffer2.length < 10) {
+        if(audioBytes1.length-offset >= 10) {
+          for(int i = 0; i < 10; i++) {
+            buffer1[i] = audioBytes1[offset+i];
+          }
+        }
+        else {
           return roundDuration(duration);
         }
-        if(buffer2[0] == 0xff && (buffer2[1] & 0xe0) == 0xe0) {
-          info = parseFrameHeader(buffer2);
+        if(buffer1[0] == 0xff && (buffer1[1] & 0xe0) == 0xe0) {
+          info = parseFrameHeader(buffer1);
           if(info[2] != 0 && info[3] != 0) {
             offset += info[2];
             duration += ( info[3] / info[1] );
+            //System.out.println(duration);
           }
           else {
             offset+=1; //Corrupt file?
           }
         }
-        else if(buffer2[0] == 0x54 && buffer2[1] == 0x41 && buffer2[2] == 0x47) { //#TAG'
-            offset += 128; //Skip over id3v1 tag size
+        else if(buffer1[0] == 0x54 && buffer1[1] == 0x41 && buffer1[2] == 0x47) { //#TAG'
+          offset += 128; //Skip over id3v1 tag size
         }
         else {
-            offset+=1; //Corrupt file?
+          offset+=1; //Corrupt file?
         }
-        if(info != null) {
-          return roundDuration(estimateDuration(info[0], offset, filesize));
-        }
-        //System.out.println("Offset: "+offset);
       }
-    } catch (FileNotFoundException ex) {
-      ex.printStackTrace();
-    }
+    } catch (FileNotFoundException e) {
 
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     return roundDuration(duration);
   }
-
-  byte[] readAndClose(InputStream aInput) {
-    // carries the data from input to output :
-    byte[] bucket = new byte[32 * 1024];
-    ByteArrayOutputStream result = null;
-    int offset = 0;
-    try {
-      try {
-        // Use buffering? No. Buffering avoids costly access to disk or network;
-        // buffering to an in-memory stream makes no sense.
-        result = new ByteArrayOutputStream(bucket.length);
-        int bytesRead = 0;
-        while (bytesRead != -1) {
-          // aInput.read() returns -1, 0, or more :
-          bytesRead = aInput.read(bucket);
-          if (bytesRead > 0) {
-            result.write(bucket, 0, bytesRead);
-          }
-        }
-      } finally {
-        aInput.close();
-        // result.close(); this is a no-operation for ByteArrayOutputStream
-      }
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    }
-    return result.toByteArray();
-  }
-
-
 }
-
--- src/main/java/MP3DurationApp.java
+++ src/main/java/MP3DurationApp.java
@@ -1,3 +1,9 @@
+/*
+ * @author Christos Giannoglou
+ * 
+ * 2018 (c) ATC
+ * 
+ */
 package main.java;
 
 public class MP3DurationApp {
@@ -8,8 +14,7 @@ public class MP3DurationApp {
     dc.setBitRates();
     dc.setSampleRates();
     dc.setSamples();
-    long duration = dc.mp3Duration("C:\\Users\\cgiannoglou\\Documents\\Source\\mp3-duration\\tests\\demo - vbr.mp3", false);
-    System.out.println("The duration of the mp3 file is: "+duration);
+    double mp3Duration = dc.mp3Duration("C:\\Users\\cgiannoglou\\Downloads\\Iron Maiden - Fear of the Dark, live Tele2 Arena, Stockholm Sweden 2018-06-01.mp3");
+    System.out.println("The duration of the mp3 file is: "+mp3Duration);
   }
-
 }
